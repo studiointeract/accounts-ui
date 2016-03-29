73af137ef059f7792f3d17e0fec505f2454b6a69
Tim Brandin <tim.brandin@gmail.com>
2016-03-29 06:22:20 +0200

    Version bump.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Accounts UI for Meteor 1.3
 
-Current version 1.0.3
+Current version 1.0.5
 
 ## Features
 
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'studiointeract:react-accounts-ui',
-  version: '1.0.4',
+  version: '1.0.5',
   summary: 'Accounts UI for React in Meteor 1.3',
   git: 'https://github.com/studiointeract/react-accounts-ui',
   documentation: 'README.md'
