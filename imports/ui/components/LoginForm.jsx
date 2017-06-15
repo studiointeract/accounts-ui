b45033b3ae1ff204d7eeda553aea2cc17d72d26a
Tim Brandin <tim.brandin@gmail.com>
2017-06-15 14:20:26 +0200

    Added missing proptypes.

--- imports/ui/components/LoginForm.jsx
+++ imports/ui/components/LoginForm.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Accounts } from 'meteor/accounts-base';
